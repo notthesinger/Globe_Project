5782f1e72c101d95dd889af4f9b0c2c33e3deca6
notthesinger <james.edmund.t@gmail.com>
2020-06-21 00:08:43 -0700

    in the middle of figuring out the scrioll problem

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask, render_template, make_response, request, url_for, redirect
+from flask import (
+    Flask,
+    render_template,
+    make_response,
+    request,
+    url_for,
+    redirect,
+    session,
+)
 
 app = Flask(__name__)
 app.debug = True
+app.secret_key = r"8gQ<fO(QBVm]*:%"
 
 
 @app.route("/")
 def home():
+    session["country"] = "not a country"
     return render_template("base.html")
 
 
@@ -14,9 +24,22 @@ def globe():
     return render_template("globe_view.html")
 
 
-@app.route("/about", methods=["GET", "POST"])
+@app.route("/about")
 def about():
     return render_template("about.html")
+
+
+@app.route("/data", methods=["POST"])
+def data_value():
+    if request.method == "POST":
+        # if session.get("country") != request.form["numdata"]:
+        country = request.form["numdata"]
+        session["country"] = country
+        with open("results.txt", "w") as f:
+            f.write(country + "\n")
+            f.write(session.get("country"))
+
+    return "fuck"
 
 
 if __name__ == "__main__":
--- static/js/getname.js
+++ static/js/getname.js
@@ -1,4 +1,10 @@
 $(document).ready(function () {
     console.log("ready!");
-    $("tspan:nth-of-type(1)").addEventListener("mouseenter",;
+    var test = "Cool"
+    var t = $("g:nth-of-type(1)").on("click", country);
+    function country(i) {
+        var num = document.querySelectorAll('tspan')[1].innerHTML;
+        console.log(i);
+        $.post("/data", { numdata: num });
+    }
 });
